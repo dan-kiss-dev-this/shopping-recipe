177aabf5865e56eb527c82a519496ed6a3f6225a
Dan Kiss <flowfast47@gmail.com>
2019-06-04 12:46:40 -0700

    add ingredient to recipe and add a dropdown with @Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,10 +22,11 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [BrowserModule, FormsModule],
-  providers: [ ShoppingListService],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/11/23/14/37/baked-goods-1853261_960_720.jpg '
+      'A Test Recipe 1',
+      'This is simply a test description 1',
+      'https://cdn.pixabay.com/photo/2016/11/23/14/37/baked-goods-1853261_960_720.jpg ',
+      [new Ingredient('Meat', 1), new Ingredient('Fries', 20)]
     ),
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/11/23/14/37/baked-goods-1853261_960_720.jpg '
+      'A Test Recipe 2',
+      'This is simply a test a test description 2',
+      'https://cdn.pixabay.com/photo/2016/11/23/14/37/baked-goods-1853261_960_720.jpg ',
+      [new Ingredient('Meat', 1), new Ingredient('Buns', 2)]
     )
   ];
 
