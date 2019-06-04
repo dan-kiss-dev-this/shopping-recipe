6bb005535df44e9e33e846db88806a2d34c4629f
Dan Kiss <flowfast47@gmail.com>
2019-06-04 12:18:37 -0700

    subscribe to changes in the ingredients list using a service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
 export class ShoppingListService {
-  
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  //ask about private here
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients;
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
